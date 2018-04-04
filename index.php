2d4cbb17e1b422d52e76c8d65c1a4e5b63d89de1
suryasivarajpalamattom <suryas@mca.ajce.in>
2018-04-04 13:19:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,358 @@
+<?php 
+include 'connection.php';
+session_start();
+
+	?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!-- Mirrored from www.cochincarrentals.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Jan 2018 04:52:30 GMT -->
+<head>
+
+<title>Easy way</title>
+
+<link rel="image_src" href="theme/images/Cochin_Car_Rentals_Safe_image_Thumbnail.jpg"/>
+<link rel="stylesheet" type="text/css" href="theme/css/header.css" />
+<link rel="shortcut icon" type="text/css" href="theme/images/fav_ico.png" />		
+<link rel="stylesheet" type="text/css" href="theme/css/layout.css" />
+<link rel="stylesheet" type="text/css" href="theme/css/form.css" />
+<link rel="stylesheet" type="text/css" href="theme/css/footer.css" />
+<link rel="stylesheet" type="text/css" href="theme/css/circlestyle.css" />
+<!--<link rel="stylesheet" type="text/css" href="theme/css/source-sans-pro/font.css" />-->
+
+
+<link rel="stylesheet" type="text/css" href="theme/css/header.css" />
+<!--<link rel="stylesheet" type="text/css" href="theme/css/source-sans-pro/font.css" />-->
+<link rel="stylesheet" type="text/css" href="theme/css/layout.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<div id="Mainheadbg">
+<div class="wrapper">
+<div id="Logo"><img src="theme/images/carlogo.png" alt="cochin car rentals" />
+</div>
+<div id="MenuBox">
+    <ul class="menu">
+        <li id="TabHome"><a href="index.php">Home</a></li>
+        <!--<li><a href="about.html" data-hover="About">About</a></li>
+        <li><a href="services.html" data-hover="Services">Services</a></li>-->
+        <li><a href="ucar.php?typeid=1">Pick Up Your Car</a></li>
+        <li><a href="register.php">Registration</a></li>
+        <li><a href="login.php">Login</a></li>
+        
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</div>
+<div class="clear"></div>
+
+</div>
+</div>
+
+
+<div class="Slidecontainer">
+  <iframe src="i_slider.php" width="100%" height="531" frameborder="0" scrolling="no"></iframe>
+</div>
+       
+<div id="CirMenu">
+<div class="wrapper">
+
+<div class="CirContainer">
+          <div class="content">
+            <ul class="ca-menu">
+              <li style="margin-left:210px;">
+                        <a href="ucar.php?typeid=1">
+                            <span class="ca-icon"><img src="theme/images/economycars_icon.png" alt="economy cars" /></span>
+                            <div class="ca-content">
+                          <h2 class="ca-main">Economy<br />Cars</h2>
+                               
+                        </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="ucar.php?typeid=2">
+                            <span class="ca-icon"><img src="theme/images/premiumcars_icon.png" alt="premium cars" /></span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">PREMIUM<br />
+                                CARS</h2>
+                              
+                        </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="ucar.php?typeid=3">
+                            <span class="ca-icon" id="heart"><img src="theme/images/luxurycars_icon.png" alt="luxury carscars" /></span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">LUXURY<br />
+                                CARS</h2>
+                             
+                        </div>
+                        </a>
+                    </li>
+                    <!--<li>
+                        <a href="large-vehicles.html">
+                            <span class="ca-icon"><img src="theme/images/largevehicles_icon.png" alt="large cars" /></span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">LUXURY<br />
+                                COACH </h2>
+                               
+                            </div>
+                        </a>
+                    </li>
+                 <li>
+                        <a href="http://keralahouseboatrentals.com/" target="_blank">
+                        <span class="ca-icon"><img src="theme/images/houseboat.png" alt="kerala houseboat rentals" /></span>
+                        <div class="ca-content">
+                        <h2 class="ca-main">HOUSEBOAT<br />
+                        RENTALS</h2>
+                               
+                            </div>
+                        </a>
+                    </li>  --> 
+                 
+             <!-- </ul>
+            </div><!-- content -->
+        </div>
+			
+
+
+
+
+</div>
+
+</div>    
+<div id="MainCont"><br><br>
+<div class="wrapper">
+
+
+<div id="ContentBox">
+<div class="WelcomeBanner">
+<div class="innercolor">
+
+<div class="WelcomeHead">
+<div id="LikeBox">
+    <!--  <div id="FB">
+       <iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FCochinCarRentals&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
+      </div>-->
+
+<div id="GPlus"> 
+        <!-- Place this tag where you want the +1 button to render. -->
+<div class="g-plusone" data-size="medium" data-href="index.html"></div>
+
+<!-- Place this tag after the last +1 button tag. -->
+
+      </div>
+      </div>
+<h2>WELCOME TO EASY WAY CAR RENTALS</h2></div>
+</div>
+</div>
+<div class="WelcomeText">
+  <p>Easy way Rentals offers low cost car hire in cochin with the best fully inclusive car hire rates from the top car rental companies. Easy way Car Renatls provides a wide choice of vehicles from budget economy options through to luxury autos. Easy way Car Rentals will compare all the top car hire providers to find you the very best deal on your car hire at Cochin.                                                                                       .<br />
+  </p><br />
+
+  <p>We offer the best deals in hiring Cars for any need of yours. Be it a car on lease for a holiday or official purpose or a bus / mini-bus for a pleasure trip or daily conveyance, Easy way Car Rentals has a choice of vehicles to suit your needs                                                                                          . </p><br />
+
+
+  <p>Our fleet of luxury cars includes Mercedes Benz, BMW, Honda Accord and Lexus to name a few. We also provide chauffeured services to our clients. Whats more Our comprehensive services come to you at affordable rates                                                                  . </p><br />
+
+</div>
+<!--
+<div id="CarSlider">
+
+    <iframe src="i_slide.html" width="612" height="220" frameborder="0" scrolling="no"></iframe>
+
+</div>-->
+</div>
+
+<div id="ReseBox">
+<div class="ReseHeadbg">
+<div class="ReseHeadtxt"><h2>MAKE A RESERVATION</h2></div>
+</div>
+<div id="EnquiryBox">
+	
+<link rel="stylesheet" type="text/css" href="theme/css/form.css" />
+<link rel="stylesheet" href="theme/css/jquery-ui.css" />
+<script src="theme/js/jquery-ui.js"></script>
+
+<script type="text/javascript">
+ $(document).ready(function(){
+    $("#Pickup").datepicker({
+		minDate: 0,
+		  numberOfMonths: 1,
+		  onSelect: function(selected) {
+          $("#Return").datepicker("option","minDate", selected)
+        }
+    });
+    $("#Return").datepicker({ 
+        minDate: 0,
+		numberOfMonths: 1,
+	    onSelect: function(selected) {
+           $("#Pickup").datepicker("option","maxDate", selected)
+        }
+    });  
+});
+</script>
+
+<form action="" method="post">
+ <p>
+    <label for="Pickup">Pickup Date</label>
+    <input name="Pickup" type="text" class="dtp" id="Pickup"  />
+  </p>
+  <p>
+    <label for="Return">Return  Date</label>
+    <input name="Return" type="text" class="dtp" id="Return"  />
+  </p>
+  <!--<p>
+     <label for="Options">Options</label>
+       <input type="radio" name="acoption" class="radio" value="AC"  id="acoption_0" />
+       <label for="acoption_0" style="padding-top:0px; padding-bottom:0px; width:67px;">A/C</label>
+        
+       <input type="radio" name="acoption" class="radio" value="NonAC" id="acoption_1"/>
+       <label for="acoption_1" style="padding-top:0px; padding-bottom:0px;">Non A/C</label>
+       
+
+  </p>-->
+  
+  
+ 
+  <p>
+  <label for="Options">Car type</label>
+                                            <!--<input type="text" placeholder="Enter your country"  class="input-xlarge" required/>-->
+                                                                            <select class="form-control" name="type_select" id="type_select" style="width:170px"; required/>
+                  <option>select</option>
+                           
+            <?php
+            $q = mysqli_query($con, "SELECT typeid,cartype FROM cartype");
+            //var_dump($q);
+
+            while ($row = mysqli_fetch_array($q)) {
+                echo '<option value=' . $row['typeid'] . '>' . $row['cartype'] . '</option>';
+            }
+            ?>
+              </select></td></td></tr>
+	 </p>
+	 <p>
+	 <label for="Options">Car Name</label>
+    <select name="car_select" id="car_select" style="width:170px"; >
+	
+     <option value="-1">select car</option></select>
+	 </select>
+	 </p>
+	 
+  <p>
+   
+  <input type="hidden" value="<?php echo $row['tid']?>" name="tid" />
+   
+    <label for="btnSubmit"> </label>
+    <input name="submit" type="submit" class="button" id="btnSubmit" value="BOOK" />
+    
+  </p>
+  <span style="color:#F00;">
+  </span>
+  
+ 
+</form>
+
+
+<?php
+//session_start();
+if(isset($_POST['submit']))
+{
+	$a=$_POST["Pickup"];
+$b=$_POST["Return"];
+
+$e=$_POST["type_select"];
+$f=$_POST["car_select"];
+            $_SESSION["cid1"]=$f;
+			$_SESSION['Pickup']=$a;
+			//echo $_SESSION['Pickup'];
+			 $_SESSION['Return']=$b;
+			 //$r=$_SESSION['Return'];
+			 $_SESSION['cartype']=$e;
+			 //$c=$_SESSION['cartype'];
+			 $_SESSION['carselect']=$f;
+			 //$cs=$_SESSION['carselect'];
+			
+			
+		  ?>
+				<script>
+					window.location.href="login.php";
+				</script>
+				<?php
+				
+				
+		//$_SESSION['book']=&$bk;		
+	
+}
+
+?>	
+
+
+<script>
+      $('body').on('change', '#type_select', function () {
+//            alert("countryslected");
+            $index = $('#type_select').val();
+           
+            $.ajax({
+            type:'post',
+                    url:'get_car.php',
+                    data:{index:$index},
+                    success:function(response)
+                    {
+//                        alert(response);
+                    console.log(response);
+                    $ar = response.split(",");
+                           $str = "<option value='-1' disabled hidden selected> </option>";
+                            for (var i = 0; i < $ar.length; i++)
+                    {
+                        $ss=$ar[i].split(':');
+                    $str += '<option value='+$ss[0]+'>' + $ss[1] + "</option>";
+                    }
+                    $('#car_select').html($str);
+                }
+                    });
+                    
+    });
+                    
+         
+                        
+                        
+		</script>		
+
+</div>
+</div>
+</div>
+
+
+
+
+
+</div>
+
+
+
+<link rel="stylesheet" type="text/css" href="theme/css/footer.css" />
+<link rel="stylesheet" type="text/css" href="theme/css/layout.css" />
+
+<div id="Footer">
+<div class="wrapper">
+</div>
+</div>
+
+
+
+<div id="EndFooter">
+<div class="wrapper">
+<div class="copyright"><p>&copy; Copyright 2018 - All Rights reserved at Easy way Car Rentals</a></p></div>
+<div class="copyright" style="text-align:right; padding-left:0px; padding-right:10px;"><p>Website designed by &nbsp; Exclusive Technologies</a></p></div>
+</div>
+</div>
+
+
+</body>
+
+<!-- Mirrored from www.cochincarrentals.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Jan 2018 04:53:35 GMT -->
+</html>
